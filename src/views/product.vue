db6cbc50b8d2f81ec8a206a55a8f8aadb04c8704
Dmitry <diman2407@bk.ru>
2024-10-30 13:56:27 +0300

    init

--- src/views/product.vue
+++ src/views/product.vue
@@ -2,17 +2,28 @@
   <div v-if="product" class="product-page">
     <div class="product-content">
       <div class="images-wrapper">
-        <div
-          :style="{backgroundImage: `url(${currentImage})`}"
-          class="big-image"/>
+        <div class="image-navigation">
+          <button @click="prevImage" class="nav-button">❮</button>
+          <div
+              class="big-image"
+              :style="{
+              backgroundImage: `url(${currentImage})`,
+              backgroundPosition: `${bgPosition.x}% ${bgPosition.y}%`,
+              backgroundSize: `${bgSize.width}% ${bgSize.height}%`
+            }"
+              @mousemove="updateBackgroundPosition"
+              @mouseleave="resetBackground"
+          />
+          <button @click="nextImage" class="nav-button">❯</button>
+        </div>
         <div class="previews">
           <div
-            v-for="img in product.images"
-            :style="{backgroundImage: `url(${img})`}"
-            @click="setImage(img)"
-            class="preview"
+              v-for="(img, index) in product.images"
+              :key="index"
+              :style="{ backgroundImage: `url(${img})` }"
+              @click="setImage(img)"
+              class="preview"
           />
-
         </div>
       </div>
       <div class="product-info">
@@ -28,7 +39,7 @@
           </div>
         </div>
         <div class="description md-subheading">
-        {{ product.description }}
+          {{ product.description }}
         </div>
         <div class="button">
           <md-button class="md-raised md-primary">В корзину</md-button>
@@ -38,87 +49,28 @@
   </div>
 </template>
 
-<style scoped lang="scss">
-  .product-page {
-    padding: 16px;
-    padding-top: 100px;
-    .product-content {
-      display: flex;
-      .images-wrapper {
-        width: 500px;
-        flex-shrink: 0;
-        margin-right: 20px;
-        .big-image {
-          width: 100%;
-          padding-top: 100%;
-          background-position: center;
-          background-size: contain;
-          margin-bottom: 20px;
-        }
-        .previews {
-          display: flex;
-          row-gap: 5px;
-          column-gap: 5px;
-          .preview {
-            width: 70px;
-            height: 70px;
-            background-position: center;
-            background-size: contain;
-            border: 1px solid transparent;
-            border-radius: 8px;
-            transition: all 0.3s ease;
-            cursor: pointer;
-            &:hover {
-              border: 1px solid black;
-            }
-          }
-        }
-      }
-      .product-info {
-        .title {
-          margin-bottom: 16px;
-        }
-        .description {
-          white-space: pre-wrap;
-        }
-      }
-
-      .prices {
-        margin-bottom: 16px;
-        .current-price {
-
-        }
-
-        .old-price {
-          text-decoration: line-through;
-          color: gray;
-        }
-      }
-      .button {
-        padding-top: 20px;
-        display: flex;
-        justify-content: flex-end;
-        width: 100%;
-      }
-    }
-  }
-</style>
 <script>
 export default {
   name: "product",
   data() {
     return {
-      currentImage: null,
+      currentImageIndex: 0, // Индекс текущего изображения
       product: null,
+      bgPosition: { x: 50, y: 50 },
+      bgSize: { width: 100, height: 100 }
+    };
+  },
+  computed: {
+    currentImage() {
+      return this.product.images[this.currentImageIndex]; // Получаем текущее изображение по индексу
     }
   },
   created() {
-    console.log(this.$route.params.productId)
-    this.getProduct()
+    this.getProduct();
   },
   methods: {
     setImage(img) {
-      this.currentImage = img
+      this.currentImageIndex = this.product.images.indexOf(img); // Устанавливаем индекс изображения
     },
     getProduct() {
       this.axios.get('http://localhost:5000/products', {
@@ -126,13 +78,134 @@ export default {
           product: this.$route.params.productId
         }
       }).then(data => {
-        this.product = data.data.data
+        this.product = data.data.data;
         if (this.product.images && this.product.images.length) {
-          this.currentImage = this.product.images[0]
+          this.currentImageIndex = 0; // Устанавливаем индекс на первое изображение
         }
-        // this.productsList = data.data.data.data.products
-      })
+      });
+    },
+    updateBackgroundPosition(event) {
+      const {offsetX, offsetY} = event;
+      const {clientWidth, clientHeight} = event.currentTarget;
+
+      this.bgPosition.x = (offsetX / clientWidth) * 100;
+      this.bgPosition.y = (offsetY / clientHeight) * 100;
+      this.bgSize.width = 150;
+      this.bgSize.height = 150;
+    },
+    resetBackground() {
+      this.bgSize.width = 100;
+      this.bgSize.height = 100;
+    },
+    prevImage() {
+      if (this.currentImageIndex > 0) {
+        this.currentImageIndex--;
+      } else {
+        this.currentImageIndex = this.product.images.length - 1; // Переключаемся на последнее изображение
+      }
+    },
+    nextImage() {
+      if (this.currentImageIndex < this.product.images.length - 1) {
+        this.currentImageIndex++;
+      } else {
+        this.currentImageIndex = 0; // Переключаемся на первое изображение
+      }
     }
   }
 }
 </script>
+
+<style scoped lang="scss">
+.product-page {
+  padding: 16px;
+  padding-top: 100px;
+
+  .product-content {
+    display: flex;
+
+    .images-wrapper {
+      width: 500px;
+      flex-shrink: 0;
+      margin-right: 20px;
+
+      .image-navigation {
+        display: flex;
+        align-items: center;
+        position: relative;
+      }
+
+      .nav-button {
+        background: none;
+        border: none;
+        font-size: 24px;
+        cursor: pointer;
+        z-index: 1;
+
+        &:hover {
+          color: gray; /* Изменяем цвет при наведении */
+        }
+      }
+
+      .big-image {
+        width: 100%;
+        padding-top: 100%;
+        background-position: center;
+        background-size: contain;
+        margin-bottom: 20px;
+        transition: background-size 0.3s ease;
+        cursor: crosshair;
+      }
+
+      .previews {
+        display: flex;
+        row-gap: 5px;
+        column-gap: 5px;
+
+        .preview {
+          width: 70px;
+          height: 70px;
+          background-position: center;
+          background-size: contain;
+          border: 1px solid transparent;
+          border-radius: 8px;
+          transition: all 0.3s ease;
+          cursor: pointer;
+
+          &:hover {
+            border: 1px solid black;
+          }
+        }
+      }
+    }
+
+    .product-info {
+      .title {
+        margin-bottom: 16px;
+      }
+
+      .description {
+        white-space: pre-wrap;
+      }
+    }
+
+    .prices {
+      margin-bottom: 16px;
+
+      .current-price {
+      }
+
+      .old-price {
+        text-decoration: line-through;
+        color: gray;
+      }
+    }
+
+    .button {
+      padding-top: 20px;
+      display: flex;
+      justify-content: flex-end;
+      width: 100%;
+    }
+  }
+}
+</style>
